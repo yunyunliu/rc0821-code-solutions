7db1d3d21bf7d7f30411df49420d45e8efb5f6e0
Yunyun Liu <celeph4is@gmail.com>
2021-08-23 10:29:15 -0700

    assign values to variables, use typeof operator and log to console

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+const fullName = 'Yunyun Liu';
+const isCool = true;
+const totalPets = 1;
+
+console.log('fullName:', fullName);
+console.log('isCool:', isCool);
+console.log('totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown = null;
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+const never = undefined;
+console.log('never:', never);
+console.log('typeof never:', never);
