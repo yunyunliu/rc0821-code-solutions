86a97260bdffe4015415d3036496c9161c49a8a8
Yunyun Liu <celeph4is@gmail.com>
2021-08-24 13:29:50 -0700

    js functions practice

---
+++ javascript-functions-practice/get-last-element.js
@@ -0,0 +1,5 @@
+/* exported getLastElement */
+function getLastElement(array) {
+  var last = array.length - 1;
+  return array[last];
+}
---
+++ javascript-functions-practice/get-right-triangle-area.js
@@ -0,0 +1,5 @@
+/* exported getRightTriangleArea */
+
+function getRightTriangleArea(base, height) {
+  return (1 / 2) * base * height;
+}
