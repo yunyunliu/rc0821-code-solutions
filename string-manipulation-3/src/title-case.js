7fa540dc4ca46c3dd6e07e6d4e40d37b01a98df8
Yunyun Liu <celeph4is@gmail.com>
2021-08-29 23:44:43 -0700

    titleCase now correctly deals with title:subtitle structure

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -10,26 +10,27 @@ at each index check
 then loop again checking for ':'
 */
 function capitalize(word) {
+  // check for exception words
+  if (word.toLowerCase() === 'javascript') {
+    return 'JavaScript';
+  }
+  if (word.toLowerCase() === 'api') {
+    return 'API';
+  }
   return word[0].toUpperCase() +
        word.slice(1);
 }
 
-function titleCase(string) {
-  const titleWords = string.split(' ');
+function capitalizeTitleParts(titlePart) {
+  const titleWords = titlePart.split(' ');
   const capitalized = [];
   const minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the',
     'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
 
   for (let i = 0; i < titleWords.length; i++) {
     const word = titleWords[i];
-    // check for exception words
-    if (word.toLowerCase() === 'javascript') {
-      capitalized.push('JavaScript');
-    } else if (word.toLowerCase() === 'api') {
-      capitalized.push('API');
-
     // check for words with hyphens
-    } else if (word.includes('-')) {
+    if (word.includes('-')) {
       const split = word.split('-');
       const first = split[0];
       const second = split[1];
@@ -37,7 +38,6 @@ function titleCase(string) {
         '-' + capitalize(second);
       capitalized.push(bothCapitalized);
     } else
-
     // check for first word in title, not minor exception word
     if ((i === 0) ||
       // (word.length > 3) ||
@@ -47,13 +47,26 @@ function titleCase(string) {
       capitalized.push(word);
     }
   }
-  const title = capitalized.join(' ');
-  if (title.includes(':')) {
+  return capitalized;
+}
+
+function titleCase(string) {
+  let title;
+  // check if for title:subtitle structure
+  if (string.includes(':')) {
     // split title into title and subtitle
-    const titleParts = title.split(': ');
-    // capitalize subtitle
+    const titleParts = string.split(': ');
+    const mainTitle = titleParts[0];
     const subtitle = capitalize(titleParts[1]);
-    return titleParts[0] + ': ' + subtitle;
+    // capitalize subtitle
+    const capSubTitle = capitalizeTitleParts(subtitle);
+    const capTitle = capitalizeTitleParts(mainTitle);
+
+    // turn arrays back into strings
+    title = capTitle.join(' ') + ': ' + capSubTitle.join(' ');
+  } else {
+    const titleArr = capitalizeTitleParts(string);
+    title = titleArr.join(' ');
   }
   return title;
 }
