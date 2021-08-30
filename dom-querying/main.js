47c8a2dd77c39806cdeb6bc584d132c34f4f4311
Yunyun Liu <celeph4is@gmail.com>
2021-08-30 11:12:18 -0700

    js dom querying

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+
+var firstH1 = document.querySelector('h1');
+console.log('log', firstH1);
+console.dir(firstH1);
+
+const idExplanation = document.querySelector('#explanation');
+console.log('log idExplanation', idExplanation);
+console.dir(idExplanation);
+
+const classHint = document.querySelector('.hint');
+console.log('log classHint', classHint);
+console.dir(classHint);
+
+const pElements = document.querySelectorAll('p');
+console.log('nodelist of p elements:', pElements);
+
+const classExampleLink = document.querySelectorAll('.example-link');
+console.log('nodelist of class example-link elements:', classExampleLink);
