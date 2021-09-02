387a2f9723c5bb1ea5b5f034fff05f7db7f3f2d1
Yunyun Liu <celeph4is@gmail.com>
2021-09-02 16:01:20 -0700

    change order of if-statement

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -3,26 +3,26 @@ const tabNodelist = document.querySelectorAll('.tab');
 const viewNodelist = document.querySelectorAll('.view');
 
 function handleTabClick({ target }) {
-  if (target.matches('.tab')) {
-    // adds .active to target, removes .active from others
-    for (let i = 0; i < tabNodelist.length; i++) {
-      const $node = tabNodelist[i];
-      // console.log('$node textContent', $node.textContent);
-      $node.textContent === target.textContent
-        ? $node.classList.add('active')
-        : $node.classList.remove('active');
-    }
+  if (!target.matches('.tab')) {
+    return;
+  }
+  // adds .active to target, removes .active from others
+  for (let i = 0; i < tabNodelist.length; i++) {
+    const $node = tabNodelist[i];
+    // console.log('$node textContent', $node.textContent);
+    $node.textContent === target.textContent
+      ? $node.classList.add('active')
+      : $node.classList.remove('active');
+  }
 
-    const targetView = target.getAttribute('data-view');
-
-    for (let i = 0; i < viewNodelist.length; i++) {
-      const $node = viewNodelist[i];
-      // console.log('$node', $node)
-      const view = $node.getAttribute('data-view');
-      view === targetView
-        ? $node.classList.remove('hidden')
-        : $node.classList.add('hidden');
-    }
+  const targetView = target.getAttribute('data-view');
+  for (let i = 0; i < viewNodelist.length; i++) {
+    const $node = viewNodelist[i];
+    // console.log('$node', $node)
+    const view = $node.getAttribute('data-view');
+    view === targetView
+      ? $node.classList.remove('hidden')
+      : $node.classList.add('hidden');
   }
 }
 
