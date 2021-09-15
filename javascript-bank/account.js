475e632c504cb87b211f94f19f4d3d38484d8939
Yunyun Liu <celeph4is@gmail.com>
2021-09-15 11:33:10 -0700

    create and add methods for transaction and account objects

---
+++ javascript-bank/account.js
@@ -0,0 +1,42 @@
+/* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (amount &&
+    typeof amount === 'number' &&
+    amount > 0 &&
+    (amount % 1 === 0)) {
+    const transaction = new Transaction('deposit', amount);
+    this.transactions.push(transaction);
+    return true;
+  }
+  return false;
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (amount &&
+    typeof amount === 'number' &&
+    amount > 0 &&
+    (amount % 1 === 0)) {
+    const transaction = new Transaction('withdrawal', amount);
+    this.transactions.push(transaction);
+    return true;
+  }
+  return false;
+};
+
+Account.prototype.getBalance = function () {
+  let balance = 0;
+  this.transactions.forEach(transaction => {
+    if (transaction.type === 'deposit') {
+      balance += transaction.amount;
+    } else {
+      balance -= transaction.amount;
+    }
+  });
+  return balance;
+};
