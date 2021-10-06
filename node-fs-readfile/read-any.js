5d1888132fca2f4368b2505a74718bb5fe1db4fd
Yunyun Liu <celeph4is@gmail.com>
2021-10-06 20:44:28 +0000

    read files using fs module

---
+++ node-fs-readfile/read-any.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+function print(name) {
+  fs.readFile(`./${name}`, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+print(fileName);
