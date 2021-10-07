7a64519e8d580665a7ace983e3a339c9a5735658
Yunyun Liu <celeph4is@gmail.com>
2021-10-07 17:49:36 +0000

    feature 2 User can add a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,10 +1,30 @@
+const fs = require('fs');
 const data = require('./data.json');
 
 const command = process.argv[2];
 const notes = data.notes;
+let note;
 
-if (command === 'read') {
-  for (const num in notes) {
-    console.log(`${num}: ${notes[num]}`);
-  }
+function writeChanges() {
+  const newData = JSON.stringify(data, null, 2);
+  fs.writeFile('./data.json', newData, (err, data) => {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  });
+}
+
+switch (command) {
+  case 'read':
+    for (const num in notes) {
+      console.log(`${num}: ${notes[num]}`);
+    }
+    break;
+  case 'create':
+    note = process.argv[3];
+    notes[data.nextId] = note;
+    data.nextId++;
+    writeChanges();
+    break;
 }
