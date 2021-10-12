ddd7c125c6a775345a200eeae5f3a5f4333647c1
Yunyun Liu <celeph4is@gmail.com>
2021-10-12 22:24:11 +0000

    sql queries

---
+++ sql-select/five-customer-emails.sql
@@ -0,0 +1,8 @@
+-- Get the customerId, firstName, lastName, and email of 5 customers.
+
+select "customerId",
+        "firstName",
+        "lastName",
+        "email"
+      from "customers"
+    limit 5;
