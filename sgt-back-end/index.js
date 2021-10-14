dc37d6a3a41abc4343d1b376ec43780535284a5d
Yunyun Liu <celeph4is@gmail.com>
2021-10-14 19:37:55 +0000

    add post request handler

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 const express = require('express');
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 // database connection
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
@@ -13,4 +13,59 @@ const db = new pg.Pool({
 
 app.listen(port, () => {
   console.log(`server @ port ${port}`);
+});
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+      SELECT *
+          FROM "grades";
+  `;
+  db.query(sql)
+    .then(data => {
+      const grades = data.rows;
+      res.json(grades);
+    })
+    .catch(err => {
+      console.log('error:', err.message);
+      res.status(500).json({ error: 'an unexpected error occurred' });
+    });
+});
+
+app.post('/api/grades', (req, res) => {
+  const { course, name, score } = req.body;
+
+  if (!name || !course || !score) {
+    res.status(400)
+      .json({ error: '"name", "course", and "score" required' });
+  } else if (Number(score) < 0 ||
+      Number(score) > 100 ||
+      !Number.isInteger(Number(score))) {
+    res.status(400)
+      .json({ error: '"score" must be an integer between 1 and 100' });
+  }
+
+  const sql = `
+      INSERT INTO "grades"
+        ("name", "course", "score")
+        values
+          ($1, $2, $3)
+        RETURNING *;
+  `;
+  const params = [name, course, score];
+  db.query(sql, params)
+    .then(data => {
+      // console.log('data:', data);
+      const [grade] = data.rows;
+      res.status(201).json(grade);
+    })
+    .catch(err => {
+      console.log('error:', err.message);
+      res.status(500).json({ error: 'unexpected error occurred' });
+    });
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const { gradeId } = req.params;
+  console.log('gradeId', gradeId);
+  res.end();
 });
