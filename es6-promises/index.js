5b1a84c78e8fc64ed8044d28d710c5eaf721f1e7
Yunyun Liu <celeph4is@gmail.com>
2021-10-14 01:01:59 +0000

    move reject callback to catch method

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,4 +2,5 @@ const takeAChance = require('./take-a-chance');
 
 const aPromise = takeAChance('Yunyun');
 
-aPromise.then(val => console.log(val), err => console.log(err.message));
+aPromise.then(val => console.log(val))
+  .catch(err => console.log(err.message));
