f7d0315729b5f7ada2b13aac2cdd5ba0a12e017d
Yunyun Liu <celeph4is@gmail.com>
2021-10-15 20:57:13 +0000

    change reduce function to check specifically for "initialValue" paramenter being undefined instead of assuming "initialValue" is undefined if it evaluates to falsy

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -1,12 +1,16 @@
+
 function reduce(array, reducer, initialValue) {
   let acc;
-  if (initialValue) {
+  // if value given for initalValue
+  if (typeof initialValue !== 'undefined') {
     acc = initialValue;
     for (let i = 0; i < array.length; i++) {
       const current = array[i];
       acc = reducer(acc, current);
     }
+    // no value passed in for initialValue
   } else {
+    // first element of array used as initialValue
     acc = array[0];
     for (let i = 1; i < array.length; i++) {
       const current = array[i];
@@ -18,7 +22,7 @@ function reduce(array, reducer, initialValue) {
 
 const reduceFunction = reduce(numbers, (acc, current) => acc + current, 0);
 console.log('sum with reduce function:', reduceFunction);
-const functionProduct = reduce(numbers, (acc, current) => acc * current);
+const functionProduct = reduce(numbers, (acc, current) => acc * current, 1);
 console.log('product with reduce function:', functionProduct);
 
 const traitsReducer = (acc, current) => {
