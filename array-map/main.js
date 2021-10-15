be870b3339372d75e50b6140e98f583501a6283f
Yunyun Liu <celeph4is@gmail.com>
2021-10-15 18:54:42 +0000

    change labels in console logs

--- array-map/main.js
+++ array-map/main.js
@@ -11,10 +11,10 @@ const languages = [
 const doubled = numbers.map(num => 2 * num);
 console.log('doubled:', doubled);
 
-const numbersWithDecimals = [1.00, 2, 3, 4, 5, 6.08, 7, 8, 9, 10.0000, 11, 12];
-console.log(numbersWithDecimals);
+const numbersWithDecimals = [1.00, 2.05, 3, 4, 5, 6.08, 7, 8.00009, 9, 10.0000, 11, 12];
+console.log('numbersWithDecimals:', numbersWithDecimals);
 const prices = numbersWithDecimals.map(num => `$${num.toFixed(2)}`);
-console.log('numbersWithDecimals:', prices);
+console.log('prices with decimals:', prices);
 const upperCased = languages.map(lang => lang.toUpperCase());
 console.log('upperCased:', upperCased);
 const firstLetters = languages.map(lang => lang[0]);
