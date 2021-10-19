8fa2193c165a11eb57e48ac26531f0ad524dde96
Yunyun Liu <celeph4is@gmail.com>
2021-10-19 17:59:55 +0000

    remove unnecessary js expression

--- react-function-components/src/index.js
+++ react-function-components/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const CustomButton = () => <button>{'Click me!'}</button>;
+const CustomButton = () => <button>Click me!</button>;
 
 ReactDOM.render(
   CustomButton(),
