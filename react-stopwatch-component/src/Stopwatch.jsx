0a593acffd01ffa560c0c330ab2666d76cfd646a
Yunyun Liu <celeph4is@gmail.com>
2021-10-20 23:07:56 +0000

    play/pause button works

--- react-stopwatch-component/src/Stopwatch.jsx
+++ react-stopwatch-component/src/Stopwatch.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 class Stopwatch extends React.Component {
   constructor() {
     super();
-    this.state = { seconds: 0 };
+    this.state = {
+      seconds: 0,
+      isOn: false,
+      timer: null
+    };
   }
 
   tick() {
@@ -11,16 +15,34 @@ class Stopwatch extends React.Component {
   }
 
   handleClick() {
-    setInterval(() => this.tick(), 1000);
+    if (this.state.isOn) {
+      // aka pause time - clear interval;
+      clearInterval(this.state.timer);
+    } else {
+      //  aka start timer -set new interval; set state.timer
+      const timer = setInterval(() => this.tick(), 1000);
+      this.setState({ timer });
+    }
+    // always toggle state
+    this.setState({ isOn: !this.state.isOn });
   }
 
   render() {
+    let icon;
+    if (this.state.isOn) {
+      icon = 'fas fa-pause';
+    } else {
+      icon = 'fas fa-play';
+    }
+
     return (
       <div className="container">
         <div className="watch">
           <span className="number">{this.state.seconds}</span>
         </div>
-        <button className="button" onClick={() => this.handleClick()}><i className="fas fa-play"></i></button>
+        <button className="button" onClick={() => this.handleClick()}>
+          <i className={icon}></i>
+        </button>
       </div>
     );
   }
