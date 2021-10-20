858d846ef905ce84124f92a43bea98285b7f3cf0
Yunyun Liu <celeph4is@gmail.com>
2021-10-20 22:36:29 +0000

    add html and styling for watchface and button

--- react-stopwatch-component/src/Stopwatch.jsx
+++ react-stopwatch-component/src/Stopwatch.jsx
@@ -3,11 +3,18 @@ import React from 'react';
 class Stopwatch extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { seconds: 0 };
   }
 
   render() {
-    return <button><i className="fas fa-play"></i></button>;
+    return (
+      <div className="container">
+        <div className="watch">
+          <span className="number">{this.state.seconds}</span>
+        </div>
+        <button className="button"><i className="fas fa-play"></i></button>
+      </div>
+    );
   }
 }
 
