bc306de7b287ab8130251917925aafbb4d075251
Yunyun Liu <celeph4is@gmail.com>
2021-10-22 17:38:36 +0000

    add validation for numbers, capital letters, and special characters

--- react-validated-input-component/src/ValidatedPassword.jsx
+++ react-validated-input-component/src/ValidatedPassword.jsx
@@ -10,19 +10,7 @@ class ValidatedPassword extends React.Component {
     };
   }
 
-  getIconClass() {
-    const length = this.state.password.length;
-    if (length < 8) {
-      return 'fas fa-times red';
-    } else if (this.state.password.length < 8) {
-      this.setState({ message: 'Password too short.' });
-      this.setState({ isValid: false });
-    } else {
-      this.setState({ isValid: true });
-    }
-  }
-
-  getMessage() {
+  validate() {
     const length = this.state.password.length;
     if (length === 0) {
       return 'Password required.';
@@ -37,7 +25,45 @@ class ValidatedPassword extends React.Component {
     this.setState({ password: e.target.value });
   }
 
+  containsChar(type) {
+    const passwordArr = this.state.password.split('');
+    let characters;
+    if (type === 'number') {
+      characters = '0123456789';
+    }
+    if (type === 'capital') {
+      characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if (type === 'special') {
+      characters = '!@#$%^&*()';
+    }
+    const filtered = passwordArr.filter(char => characters.includes(char));
+    return filtered.length > 0;
+  }
+
+  hasRequiredLength() {
+    return this.state.password.length >= 8;
+  }
+
   render() {
+    let icon = 'fas fa-times red';
+    let message = '';
+    if (this.state.password.length === 0) {
+      message = 'A password is required.';
+    }
+    if (this.hasRequiredLength) {
+      if (!this.containsChar('number')) {
+        message = 'Password must contain a number.';
+      } else if (!this.containsChar('capital')) {
+        message = 'Password must contain a capital letter.';
+      } else if (!this.containsChar('special')) {
+        message = 'Password must contain a special character (!, @, #, $, %, ^, &, *, (, or )) ';
+      } else {
+        icon = 'fas fa-check green';
+      }
+    } else {
+      message = 'Password too short';
+    }
     return (
       <>
         <div><label htmlFor="password">Password</label></div>
@@ -46,8 +72,8 @@ class ValidatedPassword extends React.Component {
             id="password"
             value={this.state.password}
             onChange={e => this.handleChange(e)}/>
-          <span> <i className={ this.state.password.length < 8 ? 'fas fa-times red' : 'fas fa-check green' }></i></span>
-          <div className="red">{this.getMessage()}</div>
+          <span> <i className={icon}></i></span>
+          <div className="red">{message}</div>
       </>
     );
   }
