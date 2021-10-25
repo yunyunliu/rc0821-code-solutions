48fd329e31dfef4ebfaa47e6167d268045f45002
Yunyun Liu <celeph4is@gmail.com>
2021-10-25 18:34:28 +0000

    remove data array from state

--- react-accordion-component/src/Accordion.jsx
+++ react-accordion-component/src/Accordion.jsx
@@ -5,50 +5,29 @@ import Item from './Item';
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: this.props.data };
+    this.state = { current: null };
     this.toggleHidden = this.toggleHidden.bind(this);
-  }
-
-  getItem(id) {
-    const arr = this.state.data;
-    const item = arr.find(el => el.id == id);
-    return item;
-
-  }
-
-  replaceItem(id, item) {
-    const arr = this.state.data;
-    const updated = arr.map(el => {
-      if (id == el.id) {
-        return item;
-      } else {
-        return el;
-      }
-    });
-    return updated;
   }
 
   toggleHidden({ target }) {
     const id = target.id;
-    const clickedItem = this.getItem(id);
-    // console.log('id:', id);
-    // console.log('clickedItem:', clickedItem)
-
-    clickedItem.isHidden = !clickedItem.isHidden;
-    const updatedData = this.replaceItem(id, clickedItem);
-
-    // console.log('updatedTechnologies:', updatedTechnologies)
-    this.setState({ data: updatedData });
+    if (id == this.state.current) {
+      this.setState({ current: null });
+    } else {
+      this.setState({ current: id });
+    }
   }
 
   render() {
-    // console.log('html:', this.state.data[0]);
     return (
       <ul className="accordion">
         {
-          this.state.data.map(el => {
+          this.props.data.map(el => {
             return (
-              <Item key={el.id} data={el} toggle={this.toggleHidden}/>
+              <Item key={el.id}
+              data={el}
+              toggle={this.toggleHidden}
+              current={this.state.current}/>
             );
           })
         }
--- react-accordion-component/src/Item.jsx
+++ react-accordion-component/src/Item.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Item = ({ data, toggle }) => {
-  const { id, isHidden, description, name } = data;
+const Item = ({ data, toggle, current }) => {
+  const { id, description, name } = data;
   return (
     <li>
       <button id={id}
          onClick={e => toggle(e)}
          type="button">{name}
       </button>
-      <p className={`description ${isHidden ? 'hidden' : ''}`}>
+      <p className={`description ${id != current ? 'hidden' : ''}`}>
         {description}
       </p>
     </li>
