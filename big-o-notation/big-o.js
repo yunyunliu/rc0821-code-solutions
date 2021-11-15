db6e19d45ddf23d44824d9f60d6a23dc7b61f0e0
Yunyun Liu <celeph4is@gmail.com>
2021-11-15 15:42:01 -0800

    corrections

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -27,17 +27,17 @@ function uniqueQuadratic(words) {
     const word = words[i];          // 2 * n = O(n)
     let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * 1 = O(1)
-      c < i;                        // 1 * n^2 = O(n^2)
-      c++                           // 2 * n^2 = O(n^2)
+      let c = 0;                    // 1 * n = O(n)
+      c < i;                        // 1 * n = O(n)
+      c++                           // 2 * n = O(n)
     ) {
       const comparing = words[c];   // 2 * n^2 = O(n^2)
       if (comparing === word) {     // 1 * n^2 = O(n^2)
         isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * n^2 = O(n^2)
-      unique[unique.length] = word; // 3 * n^2 = O(n^2)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
