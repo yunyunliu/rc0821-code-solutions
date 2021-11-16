21f05c57cbe4333cbe8a0f3da812479dab0bf451
Yunyun Liu <celeph4is@gmail.com>
2021-11-15 16:50:51 -0800

    js closures

--- javascript-closures/main.js
+++ javascript-closures/main.js
@@ -6,6 +6,8 @@ function graduate(credential) {
 
 const medicalSchool = graduate('M.D.');
 const lawSchool = graduate('Esq.');
+const gradSchool = graduate('Ph.D');
 
 console.log('medical school:', medicalSchool('Dan Abramov'));
 console.log('law School:', lawSchool('Ryan Florence'));
+console.log('grad School:', gradSchool('Ada Lovelace'));
