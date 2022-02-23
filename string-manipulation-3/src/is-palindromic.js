45f099bd101ea05f028b0ffad22ce30e2f63badc
Yunyun Liu <celeph4is@gmail.com>
2022-02-22 19:36:33 -0800

    isPalindromic, string-manip-3

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,1 +1,16 @@
 /* exported isPalindromic */
+// multiple pointer pattern
+
+function isPalindromic(string) {
+  const str = string.replaceAll(' ', '');
+  let first = 0; let last = str.length - 1;
+
+  while (first < last) {
+    if (str[first] !== str[last]) {
+      return false;
+    } else {
+      first++; last--;
+    }
+  }
+  return true;
+}
