e3f2343d1c28b7b7a05419b2f210fc9f0ad268c3
Yunyun Liu <celeph4is@gmail.com>
2022-02-22 20:51:45 -0800

    string-manip-3 reverseWords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,20 @@
 /* exported reverseWords */
+
+function reverse(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str[i];
+    reversed += char;
+  }
+
+  return reversed;
+}
+
+function reverseWords(string) {
+  const words = string.split(' ');
+  const result = [];
+  for (let i = 0; i < words.length; i++) {
+    result.push(reverse(words[i]));
+  }
+  return result.join(' ');
+}
